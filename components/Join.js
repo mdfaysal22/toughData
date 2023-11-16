e6c7db4eb46f7e516d08f56b756a295ca9c3ea49
mdfaysal22 <mdfaysal.freelancer@gmail.com>
2023-11-16 13:30:32 +0600

    Font-weight

--- components/Join.js
+++ components/Join.js
@@ -5,7 +5,7 @@ function Join() {
     <div className="bg-[#eeb7b870] py-16 my-16 w-full">
         <div className="w-[1440px] text-center mx-auto">
         <h1 className="text-[2.5rem] mb-6 text-black font-bold">Let us scale your TikTok data extraction.</h1>
-        <button className='px-5 py-3 bg-[#C34043] hover:bg-[#eeb7b870]  hover:text-[#c34043] border-[#c34043] border-2 hover:border-[#c34043] shadow-md cursor-pointer rounded-[3.25rem] font-light tracking-wider text-white text-[1.25rem]'>Sign up for free today.</button>
+        <button className='px-5 py-3 bg-[#C34043] hover:bg-[#eeb7b870]  hover:text-[#c34043] border-[#c34043] border-2 hover:border-[#c34043] shadow-md cursor-pointer rounded-[3.25rem] font-semibold tracking-wider  text-white text-[1.25rem]'>Sign up for free today.</button>
         </div>
     </div>
   )
